3678e458374068c3d909ff3cee95f544316da2a5
Simon Dell <simon.dell@dazn.com>
2025-01-13 22:27:21 +0000

    test(ordering): assert ordering the second match when higher scored

--- src/utils/orderByScoreThenIndex.test.ts
+++ src/utils/orderByScoreThenIndex.test.ts
@@ -8,15 +8,38 @@ describe('orderByScoreThenIndex', () => {
 			homeScore: 1,
 			awayTeam: 'team b',
 			awayScore: 1,
+			hasEnded: false,
 		}
 		const match2 = {
 			homeTeam: 'team c',
 			homeScore: 1,
 			awayTeam: 'team d',
 			awayScore: 0,
+			hasEnded: false,
 		}
 
 		const ordered = orderByScoreThenIndex(match1, match2);
 		assert.equal(ordered, -1);
 	})
+
+	it('returns 1 when the second match has a higher total score than the first', () => {
+		const match1 = {
+			homeTeam: 'team a',
+			homeScore: 1,
+			awayTeam: 'team b',
+			awayScore: 0,
+			hasEnded: false,
+		}
+		const match2 = {
+			homeTeam: 'team c',
+			homeScore: 1,
+			awayTeam: 'team d',
+			awayScore: 1,
+			hasEnded: false,
+		}
+
+		const ordered = orderByScoreThenIndex(match1, match2);
+		assert.equal(ordered, 1);
+	})
+
 })
