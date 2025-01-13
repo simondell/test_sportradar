c37ac11815424c1fe73a0c0f3ded9175cbc965b3
Simon Dell <simon.dell@dazn.com>
2025-01-13 16:50:33 +0000

    feat: enable ending of matches
    This adds a property  to the Match class, but not to the TMatch interface. The separation allows for type checking a restricted set of TMatch properties out of .getMatches(), whilst keeping the match data required for processing matches in the class definition. .getMatches() is updated to filter out ended matches. The Scoreboard also gains a map from home team name to the match they're playing in. This makes looking up matches by team name easier, but we must remember to remove the mapping. As these are private properties, I don't now how to assert against this behavior. It's quite important, so it should have tests, but from the perspective of core behavior, it's just an implementation detail. I might refactor it...
    
    Refs: #3

--- src/Scoreboard.ts
+++ src/Scoreboard.ts
@@ -8,18 +8,36 @@ interface TMatch {
 class Match implements TMatch {
 	public homeScore: number = 0;
 	public awayScore: number = 0;
+	public hasEnded = false;
 
 	constructor (public homeTeam: string, public awayTeam: string) {}
 }
 
 export class Scoreboard {
 	private matches: Match[] = [];
+	private matchesByHomeTeam: {[key: string]: Match} = {};
 
-	getMatches (): Match[] {
-		return this.matches;
+	endMatch (homeTeam: string) {
+		const thisMatch = this.matchesByHomeTeam[homeTeam];
+		thisMatch.hasEnded = true;
+		delete this.matchesByHomeTeam[homeTeam];
+	}
+
+	getMatches (): TMatch[] {
+		return (this.matches
+			.filter(match => !match.hasEnded)
+			.map(match => ({
+				homeTeam: match.homeTeam,
+				homeScore: match.homeScore,
+				awayTeam: match.awayTeam,
+				awayScore: match.awayScore,
+			}))
+		);
 	}
 
 	startMatch (homeTeam: string, awayTeam: string) {
-		this.matches.push(new Match(homeTeam, awayTeam));
+		const newMatch = new Match(homeTeam, awayTeam);
+		this.matchesByHomeTeam[homeTeam] = newMatch;
+		this.matches.push(newMatch);
 	}
 }
