782ccc7586b6e0e8cd817d95bced5eda3a5351ab
Simon Dell <simon.dell@dazn.com>
2025-01-13 22:38:25 +0000

    test(ordering): assert secondary order by match index

--- src/utils/orderByScoreThenIndex.test.ts
+++ src/utils/orderByScoreThenIndex.test.ts
@@ -9,18 +9,18 @@ describe('orderByScoreThenIndex', () => {
 			awayTeam: 'team b',
 			awayScore: 1,
 			hasEnded: false,
-		}
+		};
 		const match2 = {
 			homeTeam: 'team c',
 			homeScore: 1,
 			awayTeam: 'team d',
 			awayScore: 0,
 			hasEnded: false,
-		}
+		};
 
 		const ordered = orderByScoreThenIndex(match1, match2);
 		assert.equal(ordered, -1);
-	})
+	});
 
 	it('returns 1 when the second match has a higher total score than the first', () => {
 		const match1 = {
@@ -29,17 +29,38 @@ describe('orderByScoreThenIndex', () => {
 			awayTeam: 'team b',
 			awayScore: 0,
 			hasEnded: false,
-		}
+		};
 		const match2 = {
 			homeTeam: 'team c',
 			homeScore: 1,
 			awayTeam: 'team d',
 			awayScore: 1,
 			hasEnded: false,
-		}
+		};
 
 		const ordered = orderByScoreThenIndex(match1, match2);
 		assert.equal(ordered, 1);
-	})
+	});
 
-})
+	it('order by match index if scores are equal', () => {
+		const match1 = {
+			homeTeam: 'team a',
+			homeScore: 1,
+			awayTeam: 'team b',
+			awayScore: 1,
+			hasEnded: false,
+			index: 1,
+		};
+		const match2 = {
+			homeTeam: 'team c',
+			homeScore: 1,
+			awayTeam: 'team d',
+			awayScore: 1,
+			hasEnded: false,
+			index: 0,
+		};
+
+		const ordered = orderByScoreThenIndex(match1, match2);
+		assert.equal(ordered, 1);
+	});
+});
--- src/utils/orderByScoreThenIndex.ts
+++ src/utils/orderByScoreThenIndex.ts
@@ -5,5 +5,9 @@ export function orderByScoreThenIndex (match1: Match, match2: Match): -1 | 0 | 1
 	const match1Total = match1.homeScore + match1.awayScore;
 	const match2Total = match2.homeScore + match2.awayScore;
 
-	return match1Total > match2Total ? -1 : 1; 
+	return match1Total > match2Total
+		? -1
+		: match1Total === match2Total 
+		? 0
+		: 1; 
 }
