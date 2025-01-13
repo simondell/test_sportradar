67e9c52aee4bf4697cf9b1b19467e1def6102c1f
Simon Dell <simon.dell@dazn.com>
2025-01-13 13:55:40 +0000

    feat: add startMatch() method
    This adds a Match class, for easy instantiation of strctured data, as well as the .startMatch() method.
    
    refs: #1

--- src/Scoreboard.ts
+++ src/Scoreboard.ts
@@ -1,5 +1,25 @@
+interface TMatch {
+	homeTeam: string;
+	homeScore: number;
+	awayTeam: string;
+	awayScore: number;
+}
+
+class Match implements TMatch {
+	public homeScore: number = 0;
+	public awayScore: number = 0;
+
+	constructor (public homeTeam: string, public awayTeam: string) {}
+}
+
 export class Scoreboard {
-	getMatches () {
-		return [];
+	private matches: Match[] = [];
+
+	getMatches (): Match[] {
+		return this.matches;
+	}
+
+	startMatch (homeTeam: string, awayTeam: string) {
+		this.matches.push(new Match(homeTeam, awayTeam));
 	}
 }
