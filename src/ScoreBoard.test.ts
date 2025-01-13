b0c2193a9a7ee458900f6a2be2c6de4212b3f828
Simon Dell <simon.dell@dazn.com>
2025-01-13 13:36:37 +0000

    test: new match updates getMatches result

--- src/ScoreBoard.test.ts
+++ src/ScoreBoard.test.ts
@@ -7,13 +7,30 @@ describe('Scoreboard', () => {
 		const boardB = new Scoreboard();
 
 		assert.notEqual(boardA, boardB);
-	})
-})
+	});
+});
 
 describe('getMatches()', () => {
 	it('should return an empty list of matches when none have been started', () => {
 		const board = new Scoreboard();
 
 		assert.deepEqual(board.getMatches(), []);
-	})
-})
+	});
+});
+
+describe('startMatch()', () => {
+	it('should all new matches to be started', () => {
+		const board = new Scoreboard();
+
+		board.startMatch('Mock home team', 'Mock away team');
+
+		assert.deepEqual(board.getMatches(), [
+			{
+				homeTeam: 'Mock home team',
+				homeScore: 0,
+				awayTeam: 'Mock away team',
+				awayScore: 0,
+			}
+		]);
+	});
+});
