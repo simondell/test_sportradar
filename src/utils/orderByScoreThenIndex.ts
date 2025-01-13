9eaf32a9b90d742fdc05fbbdc99beb55fd471abc
Simon Dell <simon.dell@dazn.com>
2025-01-13 22:27:52 +0000

    feat(ordering): order the second match earlier when higher scored

--- src/utils/orderByScoreThenIndex.ts
+++ src/utils/orderByScoreThenIndex.ts
@@ -1,5 +1,9 @@
+import { log } from 'console';
 import { type Match } from '../Scoreboard';
 
 export function orderByScoreThenIndex (match1: Match, match2: Match): -1 | 0 | 1 {
-	return -1;
+	const match1Total = match1.homeScore + match1.awayScore;
+	const match2Total = match2.homeScore + match2.awayScore;
+
+	return match1Total > match2Total ? -1 : 1; 
 }
